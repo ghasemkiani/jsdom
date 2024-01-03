8331788ab8b5ad92fcc02e991713e410eedaa082
Ghasem Kiani <ghasemkiani@gmail.com>
2024-01-03 22:59:41 +0330

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import {JSDOM} from "jsdom";
+
+import {cutil} from "@ghasemkiani/base";
+import {DOM as DOMBase} from "@ghasemkiani/dom";
+
+class DOM extends DOMBase {
+	static {
+		cutil.extend(this, {
+			defaultText: '<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"></html>',
+			defaultMime: "application/xml",
+			_window: null,
+			get window() {
+				if (cutil.na(this._window)) {
+					this.create(this.defaultText, this.defaultMime);
+				}
+				return this._window;
+			},
+			set window(window) {
+				this._window = window;
+			},
+		});
+	}
+	static get() {
+		return this.window;
+	}
+	static create(text = this.defaultText, mime = this.defaultMime) {
+		let {window} = new JSDOM(text, {contentType: mime});
+		if (cutil.na(this._window)) {
+			this._window = window;
+		}
+		return window;
+	}
+}
+
+export {DOM};
